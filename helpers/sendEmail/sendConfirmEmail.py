3575b881d696b7676f13a834097b045e332663b4
charlykso <charlykso141@gmail.com>
2024-08-27 17:33:53 +0100

    changed email

--- helpers/sendEmail/sendConfirmEmail.py
+++ helpers/sendEmail/sendConfirmEmail.py
@@ -8,8 +8,8 @@ from ebsoft import settings
 def send_email_to_user(data, pdf_file):
     name = data['fullname'].replace(" ", "_")
     subject = '{} Application  From'.format(data['fullname'])
-    # to = 'umehlilian45@gmail.com',
-    to = 'charlykso141@gmail.com',
+    to = 'umehlilian45@gmail.com',
+    # to = 'chatwithebsoft@gmail.com',
     context = {
         'fullname': data['fullname'],
         'email': data['email'],
